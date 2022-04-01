55f8986219096377f19993308f0549f89740cf9b
Ramchandra <thakuri.ram12@gmail.com>
2022-04-01 08:36:40 +0545

    Ignoring test spec file

--- src/app/directive/font-color-changer.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FontColorChangerDirective } from './font-color-changer.directive';
-
-describe('FontColorChangerDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FontColorChangerDirective();
-    expect(directive).toBeTruthy();
-  });
-});
